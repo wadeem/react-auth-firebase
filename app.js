4f3666954b3ab68ba78d59908923a81c01ca2bb5
max3d <vadim@unikivi.ee>
2019-05-19 17:04:11 +0300

    replaced generated boilerplate in app and index

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text} from "react-native";
+
+const App = function () {
+    return <Text style={style}>Hello world!</Text>
+};
+
+export default App;
+
+const style = {
+    fontSize: 25
+};
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
+import React from "react";
+import {AppRegistry} from "react-native";
+import App from "./app.js";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent("reactNativeAuth", () => App);
