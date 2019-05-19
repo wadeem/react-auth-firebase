2705a7bc191686c497a5cbafb56af477d9b1ee9f
max3d <vadim@unikivi.ee>
2019-05-19 17:41:51 +0300

    folder import strategy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
+import {Header} from "./components/common";
 
 export default class App extends Component {
 
     render() {
         return <View>
+            <Header headerText={"Authentication"}/>
             <Text>This is a React Auth App!</Text>
         </View>
     };
---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export * from "./button.js";
+export * from "./card.js";
+export * from "./card-section.js";
+export * from "./header.js";
+
