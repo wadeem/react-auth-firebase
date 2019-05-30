791669a7fe86cad0c78aedbb46cdea6697c6d006
max3d <vadim@unikivi.ee>
2019-05-30 23:42:30 +0300

    added log out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
-import {Header} from "./components/common";
+import {Header, Button, Spinner} from "./components/common";
 import firebase from "firebase";
 import firebaseConfig from "../config/firebase.js";
 import LoginForm from "./login-form.js";
@@ -8,16 +8,41 @@ import LoginForm from "./login-form.js";
 
 export default class App extends Component {
 
+    state = {
+        isLoggedIn: null
+    };
 
     componentWillMount() {
         firebase.initializeApp(firebaseConfig);
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) this.setState({isLoggedIn: true});
+            else this.setState({isLoggedIn: false});
+        });
     }
 
+    logOut() {
+        firebase.auth().signOut()
+    }
+
+    renderContent() {
+        switch (this.state.isLoggedIn) {
+            case true:
+                return <Button handler={() => this.logOut()}>Log out</Button>
+            case false:
+                return <LoginForm/>
+            default:
+                return <Spinner/>
+        }
+
+    };
+
     render() {
-        return <View>
+        return <View style={{height: 140}}>
             <Header headerText={"Authentication"}/>
             <Text>This is a React Auth App!</Text>
-            <LoginForm/>
+            {this.renderContent()}
         </View>
-    };
-};
+    }
+    ;
+}
+;
